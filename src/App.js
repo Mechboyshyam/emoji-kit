8d087a415e04b49f15f78ee7ea729f3f307cf05f
shyam <shyamkorade0@gmail.com>
2022-12-08 22:20:21 +0530

    JS applied

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css'
 import { useState } from 'react';
 function App() {
+  const [emoji, setemoji] = useState("😍")
   return (
     <div className="emoji-container">
         <div className='title'>I'm creating this emoji project. I hope you will enjoy this 🤪</div>
 
         <div className='emoji-display'>
-              💕
+              {emoji}
         </div>
         
         <div className='emoji-list'>
-          <div className='emoji'>😍</div>
-          <div className='emoji'>🎁</div>
-          <div className='emoji'>🤑</div>
-          <div className='emoji'>🤡</div>
+          <div className='emojii' onClick={()=>{setemoji('😍')}}>😍</div>
+          <div className='emojii' onClick={()=>{setemoji('🎁')}}>🎁</div>
+          <div className='emojii' onClick={()=>{setemoji('🤑')}}>🤑</div>
+          <div className='emojii' onClick={()=>{setemoji('🤡')}}>🤡</div>
         </div>
 
         
